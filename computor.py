324d91d2734a3fe226984f9dafa6578ede6f248a
Pavel Tyshevskyi <ptyshevs@gmail.com>
2019-10-09 16:40:57 +0300

    I think, we're done here

--- computor.py
+++ computor.py
@@ -62,6 +62,8 @@ def simplify_operators(eq):
             if cnt_equal_sign > 1:
                 raise ValueError("More equal signs than necessary")
         if is_op(cur_term):
+            solved = False
+    
             left_op = eq[i - 1] if i > 0 else None
             right_op = eq[i + 1] if i < (len(eq) - 1) else None
             left_operator = is_op(left_op)
@@ -72,6 +74,7 @@ def simplify_operators(eq):
 
             if cur_operator and (type(left_op) in [None, str]) and type(right_op) is Term:
                 apply_sign(cur_operator, right_op)
+                solved = True
 
             if is_term(left_op) and is_term(right_op):
                 if cur_term == '-':
@@ -79,8 +82,10 @@ def simplify_operators(eq):
                     simplified.append("+")
                 else:
                     simplified.append(cur_term)
+                solved = True
 
-            if cur_operator and right_operator:
+            if not solved and cur_operator and left_operator:
+                print(cur_term)
                 raise ValueError("Too many operators")
 
         else:
@@ -88,7 +93,7 @@ def simplify_operators(eq):
         i += 1
     return simplified
 
-def equation_to_string(eq, add_right_side=False):
+def equation_to_string(eq, add_right_side=True):
     s =  ' + '.join((str(_) for _ in eq))
     if add_right_side:
         s += ' = 0'
@@ -113,6 +118,51 @@ def combine_same_orders(eq):
                 left.append(term)
     return sorted(left, key=lambda x: x.order, reverse=True)
 
+def solve_equation(eq):
+    """
+    For now, equation is in correct form
+    """
+    order = eq[0].order
+    if order == 0:
+        if eq[0].coef == 0:
+            return "all real values"
+        else:
+            return "no solution"
+    elif order == 1:
+        if len(eq) == 2:
+            solution = -eq[1].coef / eq[0].coef
+            return solution
+        else:
+            return 0
+
+    elif order == 2:
+        n_coefs = len(eq)
+        a = eq[0].coef
+        if n_coefs >= 2:
+            b = eq[1].coef
+        else:
+            b = 0
+        if n_coefs == 3:
+            c = eq[2].coef
+        else:
+            c = 0
+        
+        d = b ** 2 - 4 * a * c
+        print("Discriminant:", d)
+        if d > 0:
+            print("Two distinct real solutions")
+        elif d == 0:
+            print("Two same real solutions")
+        else:
+            print("No real solutions :(")
+        x1 = (-b - d ** .5) / (2 * a)
+        x2 = (-b + d ** .5) / (2 * a)
+        return x1, x2
+    else:
+        raise ValueError(f"Polynomials of degree {order} are not supported")
+    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('equation')
@@ -135,5 +185,8 @@ if __name__ == '__main__':
     print("After simplification:", ' '.join([str(_) for _ in simplified]))
     left_side = combine_same_orders(simplified)
     print("Combined sides:", ' '.join([str(_) for _ in left_side]), "= 0")
-    if len(left_side) > 3 or left_side[0].order > 2:
-        raise ValueError(f"Polynomials of degree > {len(left_side)} are not supported: {equation_to_string(left_side, True)}")
+    if len(left_side) == 0 or len(left_side) > 3 or left_side[0].order > 2:
+        raise ValueError(f"Polynomials of degree > {len(left_side)} are not supported: {equation_to_string(left_side)}")
+    solution = solve_equation(left_side)
+    print(f"{equation_to_string(left_side)}")
+    print(f"Solution: {solution}")
